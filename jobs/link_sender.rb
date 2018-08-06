6f123423042f453d5882165df2c7fc0a29f73a54
James Kinyua <jameskinyua590@gmail.com>
2018-08-06 12:30:08 +0300

    chore(refactor): Apply SRP to send_links method
    This commit refactors the send_links method to
    conform to the Single Responsibility Principle
    by moving some logic into their own helper methods.

--- jobs/link_sender.rb
+++ jobs/link_sender.rb
@@ -5,7 +5,7 @@ require_relative '../models/category'
 require_relative '../models/link'
 
 module LinkSender
-  def send_links
+  def send_mail(recipient, body)
     Pony.options = { via_options: {
       address: 'smtp.gmail.com',
       port: 587,
@@ -14,29 +14,33 @@ module LinkSender
       authentication: 'plain',
       enable_starttls_auto: true
     }, via: :smtp }
+    Pony.mail(to: recipient,
+              from: ENV['LINK_SENDER_EMAIL'],
+              subject: 'Micro learning App',
+              body: body)
+  end
 
+  def prepend_https_to_canonical_url(result_set)
+    if result_set[0]['displayUrl'].start_with?('http', 'https')
+      result_set[0]['displayUrl'].strip
+    else
+      'https://' + result_set[0]['displayUrl'].strip
+    end
+  end
+
+  def send_links
     users = User.all
     users.each do |user|
       categories = user.categories
       categories.each do |category|
         if category.active
           bing_web_search = BingService::BingWebSearchApi.new
-          parsed_json = bing_web_search.links(
-            category
-          )
-          result_set = parsed_json['webPages']['value']
-          canonical_url = result_set[0]['displayUrl'].strip
-          unless result_set[0]['displayUrl'].start_with?('http', 'https')
-            canonical_url = 'https://' + result_set[0]['displayUrl'].strip
-          end
-          link_name = category.links.find_by(
-            link: canonical_url)
+          result_set = bing_web_search.links(category)['webPages']['value']
+          canonical_url = prepend_https_to_canonical_url(result_set)
+          link_name = category.links.find_by(link: canonical_url)
           until link_name.nil?
             result_set.shuffle!
-            canonical_url = result_set[0]['displayUrl'].strip
-            unless result_set[0]['displayUrl'].start_with?('http', 'https')
-              canonical_url = 'https://' + result_set[0]['displayUrl'].strip
-            end
+            canonical_url = prepend_https_to_canonical_url(result_set)
             link_name = category.links.find_by(
               link: canonical_url
             )
@@ -47,14 +51,11 @@ module LinkSender
             snippet: result_set[0]['snippet'],
             scheduled: true
           )
-          Pony.mail(to: user.email,
-                    from: ENV['LINK_SENDER_EMAIL'],
-                    subject: 'Micro Learning App',
-                    body: "Here's today's link:\n
-                    '#{category_link.link}'\n
-                    Description:\n
-                    '#{category_link.snippet}'
-                    ")
+          email_body = "Here's today's link:\n" \
+                       "#{category_link.link}\n" \
+                       "Name:\n#{category_link.link_name}\n" \
+                       "Description:\n#{category_link.snippet}"
+          send_mail(user.email, email_body)
         end
       end
     end
